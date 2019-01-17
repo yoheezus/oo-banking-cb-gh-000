b5a6f105f9f063b56adb666907a3ae83ddc57129
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-17 19:38:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
           @amt_transferred = amount
           self.status = "complete"
       else
-          self.status = "Transaction rejected. Please check your account balance."
+          "Transaction rejected. Please check your account balance."
       end
   end
 
